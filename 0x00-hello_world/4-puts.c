381e9aa886c94320ff645f4ddc8b3612288f6903
eghianruwa, kingsley <ben.kiiz.web@gmail.com>
2022-03-10 19:24:06 -0800

    updated 4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
 #include <string.h>
 
+/**
+ * main - this is the main function
+ *
+ * Description: still the man function
+ * Return: returns 0
+ */
 int main(void)
 {	char str1[60];
-	strcpy(str1, "\n\"Programming is like building a multilingual puzzle \n");
+	strcpy(str1, "\"Programming is like building a multilingual puzzle ");
 	puts(str1);
 	return (0);
 }
