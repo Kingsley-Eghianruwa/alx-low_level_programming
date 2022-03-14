6b01af9cf615822c151898d50c5f7580fe6f54c5
eghianruwa, kingsley <ben.kiiz.web@gmail.com>
2022-03-13 19:44:17 -0700

    updated 9-print_comb.c)

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -13,8 +13,15 @@ int main(void)
 	for (ch = 48; ch <= 57; ch++)
 	{
 	putchar(ch);
-	putchar(44);
-	putchar(32);
+		if (ch != 57)
+		{
+		putchar(44);
+		putchar(32);
+		}
+		else
+		{
+		;
+		}
 	}
 	putchar('\n');
 
