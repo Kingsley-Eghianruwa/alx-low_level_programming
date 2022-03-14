3943f3134d63f3eb944f58d1a91c175bc2109808
eghianruwa, kingsley <ben.kiiz.web@gmail.com>
2022-03-13 17:23:57 -0700

    added 6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ * Description: Null
+ * Return: 0
+ */
+
+int main(void)
+{
+	int ch;
+
+	for (ch = 0; ch <= 9; ch++)
+	{
+	putchar(ch + '0');
+	}
+	putchar('\n');
+
+	return (0);
+}
