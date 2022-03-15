79742b64121d3bac2eb4c2dd699a7e7b27e20199
eghianruwa, kingsley <ben.kiiz.web@gmail.com>
2022-03-14 18:14:18 -0700

    added 5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - this is the main function
+ *
+ * Description: still the man function
+ * Return: returns 0
+ */
+int main(void)
+{	char str1[60];
+	strcpy(str1, "with proper grammer, but the outcome is a piece of art,");
+	printf("%s\n", str1);
+	return (0);
+}
