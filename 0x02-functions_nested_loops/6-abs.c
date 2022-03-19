ac92c0b9395d6a35cea81bc8f7f09240fb703760
eghianruwa, kingsley <ben.kiiz.web@gmail.com>
2022-03-19 13:26:55 -0700

    added 6.abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _abs - outputs absolute value
+ * @n: positive or negative number
+ *
+ * Description: null
+ * Return: absolute value of n
+ */
+
+int _abs(int n)
+{
+	if (n > 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return (n * -1);
+	}
+}
