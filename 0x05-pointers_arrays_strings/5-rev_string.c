36cc825154a80d23ef1b1b9c70080011a6c6d41f
eghianruwa, kingsley <ben.kiiz.web@gmail.com>
2022-03-22 20:35:17 -0700

    added 5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,65 @@
+#include "main.h"
+
+/**
+ * _strlen - finds the length of a string
+ * @s: string pointer
+ *
+ * Description: null
+ * Return: interger
+ */
+
+int _strlen(char *s)
+{
+	int count;
+
+	count = 0;
+	while (*s != '\0')
+	{
+		count++;
+		s++;
+	}
+
+	return (count);
+}
+
+/**
+ * print_rev - prints a string in reverse
+ * @s: string pointer
+ *
+ * Description: null
+ * Return: void
+ */
+
+void print_rev(char *s)
+{
+	int slen;
+
+	slen = _strlen(s) - 1;
+	while (slen >=  0)
+	{
+		_putchar(*(s + slen));
+		if (slen == 0)
+		{
+			break;
+		}
+		else
+		{
+			slen--;
+		}
+	}
+	_putchar('\n');
+}
+
+
+/**
+ * rev_string - reverses a string
+ * @s: string
+ *
+ * Description: null
+ * Return: null
+ */
+
+void rev_string(char *s)
+{
+	print_rev(s);
+}
