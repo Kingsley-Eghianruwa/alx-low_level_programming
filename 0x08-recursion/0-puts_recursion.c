05ff028a2a09daad6e3a196e6438fac33bd5e459
eghianruwa, kingsley <ben.kiiz.web@gmail.com>
2022-03-31 16:43:59 -0700

    added 0-puts_recursion.c

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,20 @@
+/**
+ * _puts_recursion - recursive puts a sting.
+ * @s: string pointer
+ *
+ * Description: null
+ * Return: null
+ */
+
+void _puts_recursion(char *s)
+{
+	_putchar(*s);
+	if (*s == '\0')
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		_puts_recursion(*(s++));
+	}
+}
