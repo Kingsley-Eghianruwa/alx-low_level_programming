f5e19ec17a70cb88197a2256f3732bea79bf2a35
eghianruwa, kingsley <ben.kiiz.web@gmail.com>
2022-04-01 01:35:55 -0700

    updated c file again

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -12,14 +12,15 @@
 
 void _puts_recursion(char *s)
 {
-	_putchar(*(s));
-	if (*s == '\0')
+	if (*s != '\0')
 	{
-		_putchar('\n');
+		_putchar(*(s));
+		_puts_recursion((s + 1));
 	}
 	else
 	{
-		_puts_recursion((s + 1));
+		_putchar('\n');
 	}
+
 }
 
