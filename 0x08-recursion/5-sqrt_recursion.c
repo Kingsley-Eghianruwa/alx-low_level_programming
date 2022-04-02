48c04e159c8d60920e4aa0dbd36d30dc0e206a54
eghianruwa, kingsley <ben.kiiz.web@gmail.com>
2022-04-02 15:58:36 -0700

    added 5-sqrt_recursion.c

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * root_finder - finds integer root by unkwon method
+ * @a: root integer
+ * @b: square integer
+ *
+ * Description: null
+ * Return: interger, -1
+ */
+
+int root_finder(int a, int b)
+{
+	if ((a * a) == b)
+	{
+		return (a);
+	}
+	else if (a == 0)
+	{
+		return (-1);
+	}
+	else
+	{
+		return (root_finder((a - 1), b));
+	}
+}
+
+/**
+ * _sqrt_recursion - driver method
+ * @n: square integer
+ *
+ * Description: null
+ * Return: integer
+ */
+
+int _sqrt_recursion(int n)
+{
+	return (root_finder((n / 2), n));
+}
+
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -8,5 +8,7 @@ void str_rev_recursion(char *s);
 int _strlen_recursion(char *s);
 int factorial(int n);
 int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+int root_finder(int a, int b);
 
 #endif
