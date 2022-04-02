bbe40ea60cc7ae763f313e3c7e1a5344a5fd6623
eghianruwa, kingsley <ben.kiiz.web@gmail.com>
2022-04-02 07:31:18 -0700

    working print rev

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * str_rev_recursion - print reverse a string via recursion
+ * @s: string pointer input
+ *
+ * Desccription: null
+ * Return: null
+ */
+
+void str_rev_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		str_rev_recursion(++s);
+		_putchar(*(s - 1));
+	}
+}
+
+
+/**
+ * _print_rev_recursion - as above
+ * @s: as above
+ *
+ * Description: null
+ * Return: void
+ */
+
+void _print_rev_recursion(char *s)
+{
+	str_rev_recursion(s);
+	_putchar('\n');
+}
+
