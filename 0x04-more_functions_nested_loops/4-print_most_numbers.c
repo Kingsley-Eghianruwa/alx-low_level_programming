9034dc6fe1e96110fba6f3dbce139955c1ed5e4d
Kingsley Eghianruwa <ben.kiiz.web@gmail.com>
2022-04-21 16:57:28 -0700

    added 4-print_most_numbers.c + main.h with function protoype for print_most_numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints numbers 0 - 9
+ * Return: null
+ */
+
+void print_most_numbers(void)
+{
+	int i;
+
+	i = 0;
+	while (i <= 9)
+	{
+		if (i != 2 || i != 4)
+		{	
+		_putchar(i + '0');
+		}
+
+		i = i + 1;
+		
+	}
+	_putchar('\n');
+}
