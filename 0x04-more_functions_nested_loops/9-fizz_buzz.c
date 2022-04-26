58f55d2983aba464b7247da0a5e3298d96c12645
Kingsley Eghianruwa <ben.kiiz.web@gmail.com>
2022-04-25 18:51:09 -0700

    fizz buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,11 +1,17 @@
 #include <stdio.h>
 #include "main.h"
+
+/**
+ * print_100 - print 1 -100 and replaces multiples of 3 and 5
+ * Return: vooid
+ */
+
 void print_100(void)
 {
 	int i;
 	char *buzz = "Buzz";
 	char *fizz = "Fizz";
-	char *fizz_buzz = "FizzBuzz";	
+	char *fizz_buzz = "FizzBuzz";
 
 	i = 1;
 	while (i <= 100)
