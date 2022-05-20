101fd8039553954ad7c6cc3d15f695edd460384a
Kingsley Eghianruwa <ben.kiiz.web@gmail.com>
2022-05-20 12:38:15 -0700

    added stddef header

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stddef.h>
 
 /**
  * _strchr - locates a char in a string
@@ -9,7 +10,7 @@
 char *_strchr(char *s, char c)
 {
 	int index = 0;
-	char *cptr = '\0';
+	char *cptr = NULL;
 
 	while (s[index] != '\0')
 	{
