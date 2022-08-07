b422baedce9f35e7b35313a954f6dace4d81cba6
Kingsley Eghianruwa <ben.kiiz.web@gmail.com>
2022-08-07 04:55:49 +0000

    added 0-malloc_checked.c + main.h + README.md

---
+++ 0x0C-more_malloc_free/main.h
@@ -0,0 +1,17 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int _putchar(char);
+int _strlen(char *s);
+int _pow(int, int);
+int _string2int(char *);
+void _putint(int);
+void _putstring(char *);
+int getdigits(int);
+
+void *malloc_checked(unsigned int);
+#endif
